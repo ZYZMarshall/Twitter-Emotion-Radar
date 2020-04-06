63a5958676648d97ec65adeb201f475f72bb1278
zyz <marshallzyz@gmail.com>
2020-04-06 15:24:52 -0400

    Update tweepy_streamer.py

--- code/tweepy_streamer.py
+++ code/tweepy_streamer.py
@@ -11,8 +11,6 @@ from tweepy import Cursor
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
- 
-import twitter_credentials
 import numpy as np
 import pandas as pd
 
